d150efcea70fd0c039282ccf5b398226d3dc3503
guanyuliu <52165252+guanyuliu@users.noreply.github.com>
2020-07-01 19:12:53 +0800

    add the TPScameraController.cs

---
+++ TPScameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPScameraController : MonoBehaviour
+{
+    //This is the simple script to set camerafollow!!
+    //--------------------------------------------------------
+
+    //First need to create the gameobject to set the camera into.
+    public Transform cameraPos; //used to save the camera position
+    void Start()
+    {
+        transform.position = cameraPos.position;
+        transform.rotation = cameraPos.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, cameraPos.position, Time.deltaTime * 3f);    //charater change position equal camera position
+        transform.rotation = Quaternion.Lerp(transform.rotation, cameraPos.rotation, Time.deltaTime * 3f); //charater change rotation equal camera rotation
+        //could let the camera buffer 
+    }
+}
